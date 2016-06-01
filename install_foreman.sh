cd587be816925bb949d74a054904a30dcfe2df7e
Joao Marcelo Barros <joaomarcelobarros@gmail.com>
2016-05-31 23:13:17 -0300

    install foreman

--- install_foreman.sh
+++ install_foreman.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #install foreman 
 apt-get install wget 
+apt-get -y install ca-certificates
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+dpkg -i puppetlabs-release-trusty.deb
 
-echo "deb http://deb.theforeman.org/ trusty 1.11" >> /etc/apt/sources.list
-echo "deb http://deb.theforeman.org/ plugins 1.11" >> /etc/apt/sources.list
-
-apt-get update -y
-
+echo "deb http://deb.theforeman.org/ trusty 1.11" > /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 1.11" >> /etc/apt/sources.list.d/foreman.list
 wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+apt-get update && apt-get -y install foreman-installer
+foreman-installer
 
-apt-get install foreman foreman-sqlite3 foreman-libvirt -y
